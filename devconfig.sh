bdedbf2de488c4b27c9664af28220a57ffedd498
mixersoft <michael@snaphappi.com>
2014-11-06 18:31:47 +0200

    debug "shell script" to configure project.. NOTE: not a real shell script, there are 2 points that ask for interactive input.

--- devconfig.sh
+++ devconfig.sh
@@ -1,25 +1,37 @@
 
+
 /* 
  * On-the-Go project config
  */
 sudo npm install -g bower ionic 
-ionic start snappi-onthego; cd snappi-onthego;
-ionic platform ios
 
+
+git clone https://github.com/mixersoft/on-the-go.git snappi-onthego
+#
+# provide github user/pass
+#
+
+
+cd snappi-onthego;
+ionic lib update
+#
+# answer 'yes'
+#
+
+ln -s ../bower_components ./www/components
+ionic platform ios
 ionic plugin add org.apache.cordova.console 
 ionic plugin add org.apache.cordova.device 
 ionic plugin add org.apache.cordova.file
 ionic plugin add com.ionic.keyboard 
 ionic plugin add me.apla.cordova.app-preferences 
-
-git clone https://github.com/mixersoft/on-the-go.git git_temp
-cp -rf ./git_temp/* .
-rm -rf ./git_temp
-
-# dependencies
+# install dependencies
 npm install
 bower install 
 
 # build
 gulp
 ionic build ios
+
+# run, or open ./platforms/ios/ion-OnTheGo.xcodeproj
+ionic emulate
