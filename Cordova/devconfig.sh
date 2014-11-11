b4db5ab71b01c4f5a97d32f7e971800fe32dab45
mixersoft <michael@snaphappi.com>
2014-11-11 22:16:37 +0200

    updated devconfig script, added additional comments. still requires interactive response in 2 places

--- Cordova/devconfig.sh
+++ Cordova/devconfig.sh
@@ -1,37 +1,65 @@
+###
+### On-the-Go:  Project Config
+###
+  
+
+### install node/nvm if not available. for OSX, see:
+###   - https://github.com/creationix/nvm
+# curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
+# nvm install stable
 
 
-#
-# On-the-Go project config
-#
-sudo npm install -g bower ionic gulp
+### required node packages, install globally:
+npm install -g bower gulp cordova ionic 
+### optional: packages for angular development
+# npm install -g grunt-cli yo generator-angular generator-angular-bootstrap generator-karma   
 
 
-#git clone https://github.com/mixersoft/on-the-go.git snappi-onthego
-#
-# provide github user/pass
-#
+### clone the github repo
+echo 'enter github user credentials'
+git clone https://github.com/mixersoft/on-the-go.git snappi-onthego
+###
+### prompt> provide github user/pass
+###
+
+### goto cordova project folder
+cd snappi-onthego/Cordova
 
 
-#cd snappi-onthego;
-ionic lib update
-#
-# answer 'yes'
-#
-
+### install dependencies
+echo '{
+  "directory": "bower_components"
+}' > .bowerrc
+### OR, add .bowerrc to github repo
+npm install
+bower install 
 ln -s /bower_components ./www/components
+### NOTE: confirm this symlink is correct!!!
+
+
+
+### install Cordova platform for ios and plugins
 ionic platform ios
 ionic plugin add org.apache.cordova.console 
 ionic plugin add org.apache.cordova.device 
 ionic plugin add org.apache.cordova.file
 ionic plugin add com.ionic.keyboard 
 ionic plugin add me.apla.cordova.app-preferences 
-# install dependencies
-npm install
-bower install 
 
-# build
-gulp
-ionic build ios
 
-# run, or open ./platforms/ios/ion-OnTheGo.xcodeproj
+### install ionic libs to ./www/lib
+echo 'answer "yes" to install ionic libs to cordova project'
+ionic lib update
+###
+### prompt> answer 'yes'
+###
+
+###
+### On-the-Go: Project Build
+###     Once the project has been successfully configured, 
+###     just run this build step
+###
+gulp; ionic build ios;
+
+### run, or open ./platforms/ios/ion-OnTheGo.xcodeproj
 #ionic emulate
