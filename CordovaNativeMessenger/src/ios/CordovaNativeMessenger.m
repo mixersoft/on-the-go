76d1dae631da8c8edc389bb1665aea83e975ea13
Ivaylo Dankolov <IvoDankolov@gmail.com>
2014-11-26 12:34:27 +0200

    More features for getPhotoById: * Returns the date created in ISO 8601 format * Returns original width and height of the asset * Supports an 'autoRotate' option to transform the image based on its EXIF tag before returning it. * Better multi-threading.

--- CordovaNativeMessenger/src/ios/CordovaNativeMessenger.m
+++ CordovaNativeMessenger/src/ios/CordovaNativeMessenger.m
@@ -16,6 +16,7 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
 
 @interface CordovaNativeMessenger () {
     NSString *callbackId;
+    NSDateFormatter *dateFormatter;
 }
 
 @end
@@ -39,10 +40,7 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
         
         NSMutableArray *resultArray = [NSMutableArray arrayWithCapacity:assets.count];
         
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
-        [dateFormatter setLocale:enUSPOSIXLocale];
-        [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
+        
         
         for(PHAsset *asset in assets) {
             [resultArray addObject:@{@"UUID":asset.localIdentifier,
@@ -64,7 +62,7 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
         
         NSObject *arg0 = command.arguments[0];
         if([arg0 isKindOfClass:[NSString class]]) {
-            identifiers = @[command.arguments.firstObject];
+            identifiers = @[command.arguments[0]];
         } else if([arg0 isKindOfClass:[NSArray class]]) {
             identifiers = (NSArray*)arg0;
         }
@@ -98,6 +96,7 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
         NSNumber *width = options[@"targetWidth"];
         NSNumber *height = options[@"targetHeight"];
         NSString *resizeModeString = options[@"resizeMode"];
+        BOOL autoRotate = [options[@"autoRotate"] boolValue];
         
         PHImageRequestOptions *requestOptions = [PHImageRequestOptions new];
         requestOptions.deliveryMode = PHVideoRequestOptionsDeliveryModeHighQualityFormat;
@@ -118,13 +117,16 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
         for(PHAsset *asset in fetchResults) {
             PHImageRequestID request = [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:imageSize contentMode:resizeMode options:requestOptions resultHandler:^(UIImage *result, NSDictionary *info) {
                 
+                __block UIImage *resultImage = result;
+                
                 NSUInteger index = [imageRequests indexOfObject:info[PHImageResultRequestIDKey]];
                 if(index == NSNotFound) {
                     NSLog(@"Warning: received orphaned image request. Should not be happening.");
                     return;
                 }
                 
-                NSString *identifier = [fetchResults[index] localIdentifier];
+                PHAsset *asset = fetchResults[index];
+                NSString *identifier = [asset localIdentifier];
                 
                 if(info[PHImageErrorKey]) {
                     NSError *err = info[PHImageErrorKey];
@@ -142,32 +144,51 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
                 }
                 
                 
-                NSData *bytes = UIImageJPEGRepresentation(result, 1);
-                NSString *base64 = [bytes base64Encoding];
-                
-                if(base64 == nil) {
-                    CDVPluginResult *pluginResult = [CDVPluginResult
-                                                     resultWithStatus:CDVCommandStatus_ERROR
-                                                     messageAsDictionary:@{@"UUID":identifier,
-                                                                           @"message":@"Base64 encoding failed"}];
+                [weakself.commandDelegate runInBackground:^{
                     
+                    if(autoRotate) {
+                        resultImage = [weakself rotateImageBasedOnEXIF:resultImage];
+                    }
+                    
+                    NSData *bytes = UIImageJPEGRepresentation(resultImage, 1);
+                    NSString *base64 = [bytes base64Encoding];
+                    
+                    if(base64 == nil) {
+                        CDVPluginResult *pluginResult = [CDVPluginResult
+                                                         resultWithStatus:CDVCommandStatus_ERROR
+                                                         messageAsDictionary:@{@"UUID":identifier,
+                                                                               @"message":@"Base64 encoding failed"}];
+                        
+                        pluginResult.keepCallback = @(requestsLeft > 1);
+                        --requestsLeft;
+                        
+                        
+                        [weakself.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                        return;
+                    }
+                    
+                    NSString *withMIME = [@"data:image/jpg;base64," stringByAppendingString:base64];
+                    
+                    NSMutableDictionary *jsonResult = [NSMutableDictionary new];
+                    jsonResult[@"UUID"] = identifier;
+                    jsonResult[@"data"] = withMIME;
+                    jsonResult[@"dateTaken"] = [dateFormatter stringFromDate:[asset creationDate]];
+                    
+                    if(autoRotate && [weakself isPortraitImage:result]) {
+                        jsonResult[@"originalWidth"] = @(asset.pixelHeight);
+                        jsonResult[@"originalHeight"] = @(asset.pixelWidth);
+                    } else {
+                        jsonResult[@"originalWidth"] = @(asset.pixelWidth);
+                        jsonResult[@"originalHeight"] = @(asset.pixelHeight);
+                    }
+                    
+                    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                                  messageAsDictionary:jsonResult];
                     pluginResult.keepCallback = @(requestsLeft > 1);
                     --requestsLeft;
-
                     
                     [weakself.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                    return;
-                }
-                
-                NSString *withMIME = [@"data:image/jpg;base64," stringByAppendingString:base64];
-                
-                CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                                              messageAsDictionary:@{@"UUID":identifier,
-                                                                                    @"data":withMIME}];
-                pluginResult.keepCallback = @(requestsLeft > 1);
-                --requestsLeft;
-                
-                [weakself.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }];
             }];
             [imageRequests addObject:@(request)];
         }
@@ -187,6 +208,12 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
 }
 
 -(void)pluginInitialize {
+    
+    dateFormatter = [[NSDateFormatter alloc] init];
+    NSLocale *enUSPOSIXLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    [dateFormatter setLocale:enUSPOSIXLocale];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onSendNativeMessage:) name:kSendNativeMessageNotification object:nil];
     
     [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
@@ -196,6 +223,92 @@ NSString *kSendNativeMessageNotification = @"com.mixersoft.on-the-go.SendNativeM
 
 -(void)dispose {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+-(BOOL)isPortraitImage:(UIImage*)image{
+    UIImageOrientation o = image.imageOrientation;
+    return o == UIImageOrientationLeft
+        || o == UIImageOrientationLeftMirrored
+        || o == UIImageOrientationRight
+        || o == UIImageOrientationRightMirrored;
+    
+}
+
+-(UIImage*)rotateImageBasedOnEXIF:(UIImage*)original{
+    
+    
+    if (original.imageOrientation == UIImageOrientationUp) return original;
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (original.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, original.size.width, original.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, original.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, original.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+    
+    switch (original.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, original.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, original.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, original.size.width, original.size.height,
+                                             CGImageGetBitsPerComponent(original.CGImage), 0,
+                                             CGImageGetColorSpace(original.CGImage),
+                                             CGImageGetBitmapInfo(original.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (original.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,original.size.height,original.size.width), original.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,original.size.width,original.size.height), original.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
 }
 
 @end
